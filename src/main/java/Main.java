a0fac17782e9352855916773231bc684c1b0c72e
Sergey Boltyshov <boltyshovs@yandex.ru>
2023-05-26 13:46:35 +0300

    main, Reader

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,30 @@
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
     static File saveFile = new File("basket.json");
 
-    public static void main(String[] args) throws FileNotFoundException {
+    static String[] products = new String[]{"Хлеб", "Яблоки", "Молоко"};
+    static int[] prices = new int[]{15, 54, 110};
+    static Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
-
-        String[] products = new String[]{"Хлеб", "Яблоки", "Молоко"};
-        int[] prices = new int[]{15, 54, 110};
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
 
         for (int i = 0; i < products.length; i++) {
             System.out.println((i + 1) + ". " + products[i] + " " + prices[i] + " руб/шт");
         }
+        ReaderXML settings = new ReaderXML(new File("shop.xml"));
+        File loadFile = new File(settings.loadFile);
+        File saveFaile = new File(settings.saveFile);
+        File logFile = new File(settings.logFile);
 
-        Basket basket = null;
+        Basket basket = createBasket(loadFile, settings.isLoad, settings.loadFormat);
         if (saveFile.exists()){
             basket = Basket.loadFromJSONFile(saveFile);
         } else {
@@ -46,6 +53,19 @@ public class Main {
         }
         basket.printCart();
 
+    }
+
+    private static Basket createBasket(File loadFile, boolean isLoad, String loadFormat) {
+        Basket basket;
+        if (isLoad && loadFile.exists()){
+            basket = switch (loadFormat){
+                case "json" -> Basket.loadFromJSONFile(loadFile);
+                case "txt" -> Basket.loadFromTxtFile(loadFile);
+                default -> new Basket(products, prices);
+            };
+        }else{
+
+        }
     }
 }
 
